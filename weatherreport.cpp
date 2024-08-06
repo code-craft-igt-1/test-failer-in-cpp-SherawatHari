49afa394fe42c14e27e5fe362435818654fd1097
Hari Singh <Hari.Singh@philips.com>
2024-08-06 13:56:55 +0530

    adding testcases to catch logical mistakes

--- weatherreport.cpp
+++ weatherreport.cpp
@@ -7,10 +7,10 @@ using std::cout, std::endl, std::string;
 namespace WeatherSpace {
 class IWeatherSensor {
  public:
-        virtual double TemperatureInC() const = 0;
-        virtual int Precipitation() const = 0;
-        virtual int Humidity() const = 0;
-        virtual int WindSpeedKMPH() const = 0;
+    virtual double TemperatureInC() const = 0;
+    virtual int Precipitation() const = 0;
+    virtual int Humidity() const = 0;
+    virtual int WindSpeedKMPH() const = 0;
 };
 
 /// This is a stub for a weather sensor. For the sake of testing
@@ -19,20 +19,31 @@ class IWeatherSensor {
 /// without needing the actual Sensor during development
 
 class SensorStub : public IWeatherSensor {
-    int Humidity() const override {
-        return 72;
+ private:
+    double temperature_;
+    int precipitation_;
+    int humidity_;
+    int windSpeed_;
+
+ public:
+    SensorStub(double temperature, int precipitation, int humidity, int windSpeed)
+        : temperature_(temperature), precipitation_(precipitation),
+          humidity_(humidity), windSpeed_(windSpeed) {}
+
+    double TemperatureInC() const override {
+        return temperature_;
     }
 
     int Precipitation() const override {
-        return 70;
+        return precipitation_;
     }
 
-    double TemperatureInC() const override {
-        return 26;
+    int Humidity() const override {
+        return humidity_;
     }
 
     int WindSpeedKMPH() const override {
-        return 52;
+        return windSpeed_;
     }
 };
 
@@ -49,6 +60,8 @@ string Report(const IWeatherSensor& sensor) {
             report = "Partly cloudy";
         else if (sensor.WindSpeedKMPH() > 50)
             report = "Alert, Stormy with heavy rain";
+        else if (precipitation >= 60)
+            report = "Rainy day";
     }
     return report;
 }
@@ -56,29 +69,40 @@ string Report(const IWeatherSensor& sensor) {
 // Test a rainy day
 
 void TestRainy() {
-    SensorStub sensor;
+    SensorStub sensor(26, 70, 72, 52);
     string report = Report(sensor);
     cout << report << endl;
-    assert(report.find("rain") != string::npos);
+    assert(report.find("Rainy") != string::npos);
 }
 
 // Test another rainy day
 
 void TestHighPrecipitationAndLowWindspeed() {
-    // This instance of stub needs to be different-
-    // to give high precipitation (>60) and low wind-speed (<50)
-    SensorStub sensor;
-
-    // strengthen the assert to expose the bug
-    // (function returns Sunny day, it should predict rain)
+    SensorStub sensor(26, 70, 72, 40);
     string report = Report(sensor);
-    assert(report.length() > 0);
+    cout << report << endl;
+    assert(report.find("Rainy") != string::npos);
+}
+
+// Test to expose the mistake
+void TestMistake() {
+    // This instance of stub is designed to expose the mistake.
+    // The logic should expose that a high temperature and high wind speed
+    // should trigger a stormy alert, but precipitation is not being handled correctly.
+    SensorStub sensor(26, 30, 72, 55);
+
+    string report = Report(sensor);
+    cout << "Report for mistake test: " << report << endl;
+
+    // Strengthen the assert to expose the bug
+    assert(report.find("Stormy") != string::npos);
 }
 }  // namespace WeatherSpace
 
 int main() {
     WeatherSpace::TestRainy();
     WeatherSpace::TestHighPrecipitationAndLowWindspeed();
+    WeatherSpace::TestMistake();
     cout << "All is well (maybe)\n";
     return 0;
 }
